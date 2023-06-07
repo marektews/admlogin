4a7b248cd234546f429f6e74b94e76c80f299269
Marek Tews <marek.tews@gmail.com>
2023-06-07 13:18:39 +0200

    Dodanie wiązania do identyfikatorów parkingowych

--- src/view/MenuView.vue
+++ src/view/MenuView.vue
@@ -1,14 +1,31 @@
 <script setup>
 
 function AdminSRA() {
-    window.location='/admin/sra'
+    window.location = '/admin/sra'
+}
+
+function AdminSRP() {
+    window.location = '/admin/srp'
 }
 </script>
 
 <template>
-    <div>
+    <div class="mylayout">
         <button class="btn btn-lg btn-outline-primary" @click="AdminSRA">
             ANKIETY AUTOKARÓW
         </button>
+
+        <button class="btn btn-lg btn-outline-info" @click="AdminSRP">
+            IDENTYFIKATORY PARKINGOWE
+        </button>
     </div>
 </template>
+
+<style scoped>
+.mylayout {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 4pt;
+}
+</style>
